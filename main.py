9a954b4cda45ae21845d994100339c6ecbc02659
ruslan <ninjaruslan@gmail.com>
2022-01-02 20:08:00 +0300

    testing bounding box

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import sys
 import ezdxf
 from ezdxf import math
-from PIL import ImageDraw
+from PIL import Image, ImageDraw
 
 
 doc = ezdxf.new('R2010')  # create a new DXF R2010 drawing, official DXF version name: 'AC1024'
@@ -50,24 +50,39 @@ class Arc_atts:
                 arcs.append([self.pts_in[i], self.pts_in[i + 1]])
         return arcs
 
+    def centers(self, i):
+        return [ezdxf.math.bulge_to_arc(ezdxf.math.Vec2(self.draw_arc()[i][0][0], self.draw_arc()[i][1][0]), ezdxf.math.Vec2(self.draw_arc()[i][0][1], self.draw_arc()[i][1][1]), self.draw_arc()[i][0][2])[0]][0]
+
     def start_angle(self, i):
-        return [ezdxf.math.bulge_to_arc(ezdxf.math.Vec2(self.draw_arc()[i][0][0], self.draw_arc()[i][1][0]), ezdxf.math.Vec2(self.draw_arc()[i][0][1], self.draw_arc()[i][1][1]), self.draw_arc()[i][0][2])[1]][0]
+        return [int(ezdxf.math.bulge_to_arc(ezdxf.math.Vec2(self.draw_arc()[i][0][0], self.draw_arc()[i][1][0]), ezdxf.math.Vec2(self.draw_arc()[i][0][1], self.draw_arc()[i][1][1]), self.draw_arc()[i][0][2])[1]*180/3.14)][0]
 
     def end_angle(self, i):
-        return [ezdxf.math.bulge_to_arc(ezdxf.math.Vec2(self.draw_arc()[i][0][0], self.draw_arc()[i][1][0]), ezdxf.math.Vec2(self.draw_arc()[i][0][1], self.draw_arc()[i][1][1]), self.draw_arc()[i][0][2])[2]][0]
+        return [int(ezdxf.math.bulge_to_arc(ezdxf.math.Vec2(self.draw_arc()[i][0][0], self.draw_arc()[i][1][0]), ezdxf.math.Vec2(self.draw_arc()[i][0][1], self.draw_arc()[i][1][1]), self.draw_arc()[i][0][2])[2]*180/3.14)][0]
+
+    def radius(self, i):
+        return [ezdxf.math.bulge_to_arc(ezdxf.math.Vec2(self.draw_arc()[i][0][0], self.draw_arc()[i][1][0]), ezdxf.math.Vec2(self.draw_arc()[i][0][1], self.draw_arc()[i][1][1]), self.draw_arc()[i][0][2])[3]][0]
 
     def bounding_box(self, i):
-        return [int(self.draw_arc()[i][0][0]*self.res), int(self.draw_arc()[i][0][1]*self.res), int(self.draw_arc()[i][1][0]*self.res), int(self.draw_arc()[i][1][1]*self.res)]
+        return list(map(lambda x: int((x-self.radius(i))), self.centers(i))) + list(map(lambda x: int((x+self.radius(i))), self.centers(i)))
 
 
+res = 10
+
+t = Arc_atts(pts, res)
+
+print(t.centers(1))
+print(t.bounding_box(1))
+
+out = Image.new("RGB", (400, 400), (255, 255, 255))
 
 
-t = Arc_atts(pts, 100)
+d = ImageDraw.Draw(out)
 
-print(t.draw_arc())
 
-print(t.bounding_box(0))
-print(t.start_angle(0))
+d.ellipse(t.bounding_box(0),t.start_angle(0), t.end_angle(0))
+
+out.show()
+
 
 
 
