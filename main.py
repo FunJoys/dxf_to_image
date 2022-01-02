0e913536d7ffc2c680b905169072798a0153dbd3
ruslan <ninjaruslan@gmail.com>
2022-01-02 19:35:45 +0300

    returned to Pillow

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import cv2
+
 import sys
 import ezdxf
 from ezdxf import math
-import numpy as np
+from PIL import ImageDraw
+
 
 doc = ezdxf.new('R2010')  # create a new DXF R2010 drawing, official DXF version name: 'AC1024'
 
@@ -38,8 +39,9 @@ for i in msp:
 
 class Arc_atts:
 
-    def __init__(self, pts_in):
+    def __init__(self, pts_in, res):
         self.pts_in = pts_in
+        self.res = res
 
     def draw_arc(self):
         arcs = []
@@ -48,43 +50,25 @@ class Arc_atts:
                 arcs.append([self.pts_in[i], self.pts_in[i + 1]])
         return arcs
 
-    def centers(self, i):
-        return [ezdxf.math.bulge_to_arc(ezdxf.math.Vec2(self.draw_arc()[i][0][0], self.draw_arc()[i][1][0]), ezdxf.math.Vec2(self.draw_arc()[i][0][1], self.draw_arc()[i][1][1]), self.draw_arc()[i][0][2])[0]][0]
+    def start_angle(self, i):
+        return [ezdxf.math.bulge_to_arc(ezdxf.math.Vec2(self.draw_arc()[i][0][0], self.draw_arc()[i][1][0]), ezdxf.math.Vec2(self.draw_arc()[i][0][1], self.draw_arc()[i][1][1]), self.draw_arc()[i][0][2])[1]][0]
 
-    def start_angles(self, i):
-        return [ezdxf.math.bulge_to_arc(ezdxf.math.Vec2(self.draw_arc()[i][0][0], self.draw_arc()[i][1][0]), ezdxf.math.Vec2(self.draw_arc()[i][0][1], self.draw_arc()[i][1][1]), self.draw_arc()[i][0][2])[1]]
+    def end_angle(self, i):
+        return [ezdxf.math.bulge_to_arc(ezdxf.math.Vec2(self.draw_arc()[i][0][0], self.draw_arc()[i][1][0]), ezdxf.math.Vec2(self.draw_arc()[i][0][1], self.draw_arc()[i][1][1]), self.draw_arc()[i][0][2])[2]][0]
 
-
-
-
-
+    def bounding_box(self, i):
+        return [int(self.draw_arc()[i][0][0]*self.res), int(self.draw_arc()[i][0][1]*self.res), int(self.draw_arc()[i][1][0]*self.res), int(self.draw_arc()[i][1][1]*self.res)]
 
 
-t = Arc_atts(pts)
+
+
+t = Arc_atts(pts, 100)
 
 print(t.draw_arc())
 
-print(t.start_angles(1))
+print(t.bounding_box(0))
+print(t.start_angle(0))
 
-
-
-
-
-
-
-
-
-
-
-# height = 50
-# width = 50
-#
-# img = np.zeros((height, width, 3), np.uint8)
-#
-# img[:, 0:width] = (255, 255, 255)      # (B, G, R)
-#
-# cv2.imshow('image', img)
-# cv2.waitKey(0)
 
 
 
