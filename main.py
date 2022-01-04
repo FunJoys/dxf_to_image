d7d93ed7c7cb9f3e6906e2139a4489db65b6ff4b
ruslan <ninjaruslan@gmail.com>
2022-01-05 00:21:41 +0300

    ezdxf_done_one_polyline

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 import sys
 import ezdxf
 from ezdxf import math
-from PIL import ImageDraw
+from ezdxf import recover
+from ezdxf.addons.drawing import matplotlib
+
 
 
 doc = ezdxf.new('R2010')  # create a new DXF R2010 drawing, official DXF version name: 'AC1024'
@@ -10,7 +12,7 @@ doc = ezdxf.new('R2010')  # create a new DXF R2010 drawing, official DXF version
 msp = doc.modelspace()  # add new entities to the modelspace
 
 
-msp.add_lwpolyline([(0, 0, 0), (10, 0, 1), (20, 0, 0), (20, 20, 1), (0, 20, 0), (0, 0, 0)], format='xyb')
+msp.add_lwpolyline([(0, 0, 0), (10, 0, 1), (20, 0, 0), (20, 20, 1), (0, 20, 0), (0, 0, 0)], format='xyb', close=True)
 
 
 doc.saveas('lwpoly.dxf')
@@ -25,11 +27,10 @@ except ezdxf.DXFStructureError:
     print(f'Invalid or corrupted DXF file.')
     sys.exit(2)
 
-msp = doc1.modelspace()
+
 
 
 pts = []
-
 
 
 for i in msp:
@@ -37,43 +38,24 @@ for i in msp:
         pts = i.get_points('xyb')
 
 
-class Arc_atts:
 
-    def __init__(self, pts_in, res):
-        self.pts_in = pts_in
-        self.res = res
+doc2 = ezdxf.new('R2000')  # hatch requires the DXF R2000 (AC1015) format or later
+msp2 = doc2.modelspace()  # adding entities to the model space
 
-    def draw_arc(self):
-        arcs = []
-        for i in range(len(self.pts_in)):
-            if self.pts_in[i][-1] != 0:
-                arcs.append([self.pts_in[i], self.pts_in[i + 1]])
-        return arcs
+hatch = msp2.add_hatch(color=1, dxfattribs={
+    'hatch_style': ezdxf.const.HATCH_STYLE_NESTED})
 
-    def start_angle(self, i):
-        return [ezdxf.math.bulge_to_arc(ezdxf.math.Vec2(self.draw_arc()[i][0][0], self.draw_arc()[i][1][0]), ezdxf.math.Vec2(self.draw_arc()[i][0][1], self.draw_arc()[i][1][1]), self.draw_arc()[i][0][2])[1]][0]
+hatch.paths.add_polyline_path(
+    pts, is_closed=True,
+    flags=ezdxf.const.BOUNDARY_PATH_EXTERNAL)
 
-    def end_angle(self, i):
-        return [ezdxf.math.bulge_to_arc(ezdxf.math.Vec2(self.draw_arc()[i][0][0], self.draw_arc()[i][1][0]), ezdxf.math.Vec2(self.draw_arc()[i][0][1], self.draw_arc()[i][1][1]), self.draw_arc()[i][0][2])[2]][0]
-
-    def bounding_box(self, i):
-        return [int(self.draw_arc()[i][0][0]*self.res), int(self.draw_arc()[i][0][1]*self.res), int(self.draw_arc()[i][1][0]*self.res), int(self.draw_arc()[i][1][1]*self.res)]
+doc2.saveas("solid_hatch_polyline_path_with_bulge.dxf")
 
 
-
-
-t = Arc_atts(pts, 100)
-
-print(t.draw_arc())
-
-print(t.bounding_box(0))
-print(t.start_angle(0))
-
-
-
-
-
-
+# Exception handling left out for compactness:
+doc, auditor = recover.readfile("solid_hatch_polyline_path_with_bulge.dxf")
+if not auditor.has_errors:
+    matplotlib.qsave(doc.modelspace(), 'solid_hatch_polyline_path_with_bulge.png')
 
 
 
