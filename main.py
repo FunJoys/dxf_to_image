32bfe4fb2b8701e5333b6065778ba5b19a1631ef
ruslan <ninjaruslan@gmail.com>
2022-01-05 02:11:33 +0300

    ezdxf_done_many_polylines

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 
 import sys
 import ezdxf
-from ezdxf import math
+import matplotlib.pyplot as plt
 from ezdxf import recover
-from ezdxf.addons.drawing import matplotlib
-
-
-
-doc = ezdxf.new('R2010')  # create a new DXF R2010 drawing, official DXF version name: 'AC1024'
-
-msp = doc.modelspace()  # add new entities to the modelspace
-
-
-msp.add_lwpolyline([(0, 0, 0), (10, 0, 1), (20, 0, 0), (20, 20, 1), (0, 20, 0), (0, 0, 0)], format='xyb', close=True)
-
-
-doc.saveas('lwpoly.dxf')
+from ezdxf.addons.drawing import RenderContext, Frontend
+from ezdxf.addons.drawing.matplotlib import MatplotlibBackend
 
 
 try:
-    doc1 = ezdxf.readfile("lwpoly.dxf")
+    doc1 = ezdxf.readfile("weibo.dxf")
 except IOError:
     print(f'Not a DXF file or a generic I/O error.')
     sys.exit(1)
@@ -27,17 +16,16 @@ except ezdxf.DXFStructureError:
     print(f'Invalid or corrupted DXF file.')
     sys.exit(2)
 
-
-
+msp = doc1.modelspace()  # add new entities to the modelspace
 
 pts = []
 
 
 for i in msp:
-    if i.dxftype() == 'LWPOLYLINE':
-        pts = i.get_points('xyb')
+    if i.dxftype() == 'LWPOLYLINE' and i.dxf.layer == 'Top':
+        pts.append(i.get_points('xyb'))
 
-
+print(pts)
 
 doc2 = ezdxf.new('R2000')  # hatch requires the DXF R2000 (AC1015) format or later
 msp2 = doc2.modelspace()  # adding entities to the model space
@@ -45,17 +33,17 @@ msp2 = doc2.modelspace()  # adding entities to the model space
 hatch = msp2.add_hatch(color=1, dxfattribs={
     'hatch_style': ezdxf.const.HATCH_STYLE_NESTED})
 
-hatch.paths.add_polyline_path(
-    pts, is_closed=True,
-    flags=ezdxf.const.BOUNDARY_PATH_EXTERNAL)
-
-doc2.saveas("solid_hatch_polyline_path_with_bulge.dxf")
+for i in range(len(pts)):
+    hatch.paths.add_polyline_path(pts[i], is_closed=True, flags=ezdxf.const.BOUNDARY_PATH_EXTERNAL)
 
 
-# Exception handling left out for compactness:
-doc, auditor = recover.readfile("solid_hatch_polyline_path_with_bulge.dxf")
-if not auditor.has_errors:
-    matplotlib.qsave(doc.modelspace(), 'solid_hatch_polyline_path_with_bulge.png')
 
+
+fig = plt.figure()
+ax = fig.add_axes([0, 0, 1, 1])
+ctx = RenderContext(doc2)
+out = MatplotlibBackend(ax)
+Frontend(ctx, out).draw_layout(doc2.modelspace(), finalize=True)
+fig.savefig('your.png', dpi=300)
 
 
