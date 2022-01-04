f79a2395345efde53f46781f4e1bedfe15946871
ruslan <ninjaruslan@gmail.com>
2022-01-05 02:43:34 +0300

    added_class_and_draw

--- main.py
+++ main.py
@@ -30,27 +30,34 @@ class GetData:
         return pts
 
 
+class DrawDxf:
+    def __init__(self, pts, dpi=300):
+        self.pts = pts
+        self.dpi = dpi
+        self.Draw()
+
+    def Draw(self):
+        doc2 = ezdxf.new('R2000')  # hatch requires the DXF R2000 (AC1015) format or later
+        msp2 = doc2.modelspace()  # adding entities to the model space
+        hatch = msp2.add_hatch(color=1, dxfattribs={
+            'hatch_style': ezdxf.const.HATCH_STYLE_NESTED})
+        for i in range(len(self.pts)):
+            hatch.paths.add_polyline_path(self.pts[i], is_closed=True, flags=ezdxf.const.BOUNDARY_PATH_EXTERNAL)
+
+        fig = plt.figure()
+        ax = fig.add_axes([0, 0, 1, 1])
+        ctx = RenderContext(doc2)
+        out = MatplotlibBackend(ax)
+        Frontend(ctx, out).draw_layout(doc2.modelspace(), finalize=True)
+        fig.savefig('your.png', dpi=self.dpi)
+
+
+
+
+
 dxfname = "weibo.dxf"
 dxf1 = GetData(dxfname)
 pts = dxf1.get_polyline_data()
+DrawDxf(pts, dpi=450)
 
-doc2 = ezdxf.new('R2000')  # hatch requires the DXF R2000 (AC1015) format or later
-msp2 = doc2.modelspace()  # adding entities to the model space
-
-hatch = msp2.add_hatch(color=1, dxfattribs={
-    'hatch_style': ezdxf.const.HATCH_STYLE_NESTED})
-
-for i in range(len(pts)):
-    hatch.paths.add_polyline_path(pts[i], is_closed=True, flags=ezdxf.const.BOUNDARY_PATH_EXTERNAL)
-
-
-def draw_dxf(doc, dpi):
-    fig = plt.figure()
-    ax = fig.add_axes([0, 0, 1, 1])
-    ctx = RenderContext(doc)
-    out = MatplotlibBackend(ax)
-    Frontend(ctx, out).draw_layout(doc.modelspace(), finalize=True)
-    fig.savefig('your.png', dpi=dpi)
-
-draw_dxf(doc2, dpi=300)
 
